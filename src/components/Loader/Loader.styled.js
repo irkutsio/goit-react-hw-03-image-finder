254b13538127b26c25afb5f706cc45e4892460ab
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-06-15 00:08:22 +0300

    Update Loader.styled.js

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
-
-
 export const Loader = styled.div`
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
 `;
